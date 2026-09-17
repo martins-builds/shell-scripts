5d6c0fb434f9a053d6a84c52b7fbe29af79debd6
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-17 14:41:32 +0100

    Create cond.sh

---
+++ dev/cond.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+s=2
+d=2
+
+if [[ $s == $d ]]; then
+        echo 's and d are the same'
+fi
+
+f=2
+g=3
+
+if [[ $f == $g ]]; then
+        echo 'f and g are the same'
+else
+        echo 'f and g are not the same'
+fi
